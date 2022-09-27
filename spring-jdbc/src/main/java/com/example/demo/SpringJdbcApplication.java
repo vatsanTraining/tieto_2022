2b3622b39abcb00a13c4699f59c0f1dfe5731454
srivatsan <vatsan_k@hotmail.com>
2022-09-27 12:32:55 +0530

    Main Method Completed

--- spring-jdbc/src/main/java/com/example/demo/SpringJdbcApplication.java
+++ spring-jdbc/src/main/java/com/example/demo/SpringJdbcApplication.java
@@ -2,12 +2,30 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.example.demo.model.Customer;
+import com.example.demo.services.CustomerService;
 
 @SpringBootApplication
 public class SpringJdbcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringJdbcApplication.class, args);
+	 ConfigurableApplicationContext	 ctx =SpringApplication.run(SpringJdbcApplication.class, args);
+	
+	  Customer obj = ctx.getBean(Customer.class);
+	  
+	    obj.setCustomerId(Integer.parseInt(args[0]));
+	    obj.setCustomerName(args[1]);
+	    obj.setEmail(args[2]);
+	
+	  CustomerService service = ctx.getBean(CustomerService.class);
+	  
+	   System.out.println("Is Added :="+ service.save(obj));
+	   
+	 
+	 ctx.close();
+	
 	}
 
 }
