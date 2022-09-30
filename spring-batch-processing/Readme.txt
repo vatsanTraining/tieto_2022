76e845027a1be297cc4989546f80499abe0b3f24
srivatsan <vatsank@gmail.com>
2022-09-30 14:31:53 +0530

    Update

--- spring-batch-processing/Readme.txt
+++ spring-batch-processing/Readme.txt
@@ -3,7 +3,8 @@ Done => Add web,lombok,data jpa,mysql driver and spring batch modules
 Done => Create a Entity class 
 Done => Create a Interface extending JPA repository
 Done => Update the application.properties to applicatio.yml and add jpa related entries
-Create a Configuration class and annotate it with @Configuration
+Done => Create a Configuration class and annotate it with @Configuration
+        Create a .csv File with few lines of data
 In the Configuration Class Register Beans of following types 
   ItemReader,ItemWriter,ItemProcessor,Job and Step
 From the Main method Access Job Launcher and run the application  
--- spring-batch-processing/src/main/java/com/example/demo/config/BatchConfig.java
+++ spring-batch-processing/src/main/java/com/example/demo/config/BatchConfig.java
@@ -5,9 +5,14 @@ import java.util.List;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
 import com.example.demo.entity.Student;
 import com.example.demo.repos.StudentRepository;
@@ -52,5 +57,39 @@ public class BatchConfig {
 			 return student;
 
 		 };
+	}
+	
+	@Bean
+	public DefaultLineMapper<Student> mapper(){
+			
+		DefaultLineMapper<Student> mapper = new DefaultLineMapper<>();
+		
+		DelimitedLineTokenizer tokenizer = 
+				  new DelimitedLineTokenizer();
+		
+		tokenizer.setNames("rollNumber","studentName","markScored");
+		
+		
+		BeanWrapperFieldSetMapper<Student> fieldMapper = 
+				   new BeanWrapperFieldSetMapper<>();
+		
+		fieldMapper.setTargetType(Student.class);
+		
+		
+		mapper.setLineTokenizer(tokenizer);
+		mapper.setFieldSetMapper(fieldMapper);
+		
+		
+		return mapper;
+	}
+	
+	@Bean
+	public FlatFileItemReader<Student>  reader(){
+		
+		FlatFileItemReader<Student> reader = new FlatFileItemReader<>();
+		
+		 reader.setLineMapper(mapper());
+		 
+		 return reader;
 	}
 }
